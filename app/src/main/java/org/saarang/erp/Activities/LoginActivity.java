3a0f2fab44df64db2598355ed2a8ab48523928e8
Aqel Ahammad <aqel123@gmail.com>
2015-06-16 14:24:58 +0530

    First commit

--- app/src/main/java/org/saarang/erp/Activities/LoginActivity.java
+++ app/src/main/java/org/saarang/erp/Activities/LoginActivity.java
@@ -2,20 +2,32 @@ package org.saarang.erp.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.saarang.erp.R;
+import org.saarang.saarangsdk.Network.GetRequest;
 
 
 public class LoginActivity extends Activity {
 
     Button bLogin;
+    private static String LOG_TAG = "LoginActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_login);
+
+        new GetCoverPic().execute();
+        /**
+         * Login Button
+         */
         bLogin = (Button) findViewById(R.id.bLogin);
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +37,38 @@ public class LoginActivity extends Activity {
             }
         });
     }
+
+    /**
+     * AsyncTask for getting the cover pic of the user.
+     */
+    private class GetCoverPic extends AsyncTask<Void, Void, Void> {
+
+        boolean ifContinue = true;
+
+        @Override
+        protected Void doInBackground(Void... aVoid) {
+            String urlString = "https://graph.facebook.com/me?fields=cover&access_token=34baa";
+//            Log.d(LOG_TAG, urlString);
+            try {
+                JSONObject responseJSON = GetRequest.execute(urlString, null);
+                if (responseJSON != null) {
+                    Log.d(LOG_TAG, responseJSON.toString());
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e(LOG_TAG, "Get cover pic failed");
+            }
+            if (isCancelled()) ifContinue = false;
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+        }
+    }
+
+
 
 }
