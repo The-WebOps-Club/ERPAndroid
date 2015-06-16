dd5bc38d01318a6be857b1b5dd0e8976aab19683
Aqel Ahammad <aqel123@gmail.com>
2015-06-17 01:43:54 +0530

    Added tab layout and navigation drawer

--- app/src/main/java/org/saarang/erp/Activities/MainActivity.java
+++ app/src/main/java/org/saarang/erp/Activities/MainActivity.java
@@ -1,96 +1,156 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.saarang.erp.Activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 
-import org.saarang.erp.Adapters.ViewPagerAdapter;
 import org.saarang.erp.Fragments.NewsFeedFragment;
+import org.saarang.erp.Fragments.NotificationsFragment;
+import org.saarang.erp.Fragments.PeopleFragment;
+import org.saarang.erp.Fragments.WallsFragment;
 import org.saarang.erp.R;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+import java.util.ArrayList;
+import java.util.List;
 
-    ImageView[] imageViews;
-    ViewPager viewPager;
-    int[] drawableSelected, drawableDefault;
+/**
+ * TODO
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ac_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new NewsFeedFragment())
-                    .commit();
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
         }
-        imageViews = new ImageView[]{ (ImageView)findViewById(R.id.iv1), (ImageView)findViewById(R.id.iv2), (ImageView)findViewById(R.id.iv3), (ImageView)findViewById(R.id.iv4) };
-        for (int i=0; i<imageViews.length; i++){
-            imageViews[i].setOnClickListener(this);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
         }
-        drawableDefault = new int[]{R.drawable.ic_newsfeed, R.drawable.ic_notifications , R.drawable.ic_wall,  R.drawable.ic_people};
-        drawableSelected = new int[]{R.drawable.ic_newsfeed_selected, R.drawable.ic_notifications_selected , R.drawable.ic_wall_selected,  R.drawable.ic_people_selected};
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPageScrolled(int i, float v, int i2) {
-
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-                changeIcons(i);
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-    }
 
-    private void changeIcons(int position) {
-        for (int i = 0; i<4; i++){
-            imageViews[i].setImageResource(drawableDefault[i]);
-        }
-        imageViews[position].setImageResource(drawableSelected[position]);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setupWithViewPager(viewPager);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ac_main, menu);
+        getMenuInflater().inflate(R.menu.sample_actions, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new NewsFeedFragment(), "News Feed");
+        adapter.addFragment(new NotificationsFragment(), "Notifications");
+        adapter.addFragment(new WallsFragment(), "Walls");
+        adapter.addFragment(new PeopleFragment(), "People");
+        viewPager.setAdapter(adapter);
+    }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.iv1:
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.container, new NewsFeedFragment())
-                        .commit();
-                break;
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
         }
     }
 }
