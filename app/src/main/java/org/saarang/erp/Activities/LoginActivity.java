aa27806372121f6e7d700743757fd8c661b9b63a
Aqel Ahammad <aqel123@gmail.com>
2015-06-16 19:50:57 +0530

    First commit

--- app/src/main/java/org/saarang/erp/Activities/LoginActivity.java
+++ app/src/main/java/org/saarang/erp/Activities/LoginActivity.java
@@ -1,71 +1,97 @@
 package org.saarang.erp.Activities;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.saarang.erp.R;
-import org.saarang.saarangsdk.Network.GetRequest;
+import org.saarang.erp.Utils.UIUtils;
+import org.saarang.erp.Utils.URLConstants;
+import org.saarang.saarangsdk.Network.Connectivity;
+import org.saarang.saarangsdk.Network.PostRequest;
+import org.saarang.saarangsdk.Objects.PostParam;
+
+import java.util.ArrayList;
 
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity {a
 
     Button bLogin;
     private static String LOG_TAG = "LoginActivity";
+    LinearLayout llMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_login);
 
-        new GetCoverPic().execute();
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+
         /**
-         * Login Button
+         * Login Butto
          */
         bLogin = (Button) findViewById(R.id.bLogin);
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                //Checking for connection
+                if (Connectivity.isConnected()){
+                    new Login().execute();
+                } else {
+                    UIUtils.showSnackBar(v, getResources().getString(R.string.error_connection));
+                }
             }
         });
     }
 
     /**
-     * AsyncTask for getting the cover pic of the user.
+     * AsyncTask for Logging in .
      */
-    private class GetCoverPic extends AsyncTask<Void, Void, Void> {
+    private class Login extends AsyncTask<Void, Void, Void> {
 
-        boolean ifContinue = true;
+        ArrayList<PostParam> params = new ArrayList<PostParam>();
+        ProgressDialog pDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Logging in ...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
 
         @Override
         protected Void doInBackground(Void... aVoid) {
-            String urlString = "https://graph.facebook.com/me?fields=cover&access_token=34baa";
-//            Log.d(LOG_TAG, urlString);
-            try {
-                JSONObject responseJSON = GetRequest.execute(urlString, null);
-                if (responseJSON != null) {
-                    Log.d(LOG_TAG, responseJSON.toString());
 
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Log.e(LOG_TAG, "Get cover pic failed");
+            String urlString = URLConstants.SERVER + URLConstants.URL_LOGIN;
+
+            //Adding Parameters
+            params.add(new PostParam("email", "deepakpadamata@gmail.com"));
+            params.add(new PostParam("password", "saarang"));
+            params.add(new PostParam("deviceId", "password"));
+
+            //Making request
+            JSONObject responseJSON = PostRequest.execute(urlString, params, null);
+            if (responseJSON == null) {
+                return null;
             }
-            if (isCancelled()) ifContinue = false;
+
+
+            Log.d(LOG_TAG, responseJSON.toString());
             return null;
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
-
+            pDialog.dismiss();
         }
     }
 
