2fbcfa903e278b16dbb5f32b0f8e5bd496bfe347
Aqel Ahammad <aqel123@gmail.com>
2015-06-16 22:43:38 +0530

    Added UIUtils

---
+++ app/src/main/java/org/saarang/erp/Utils/UIUtils.java
@@ -0,0 +1,22 @@
+package org.saarang.erp.Utils;
+
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Ahammad on 16/06/15.
+ */
+public class UIUtils {
+
+    public static void showSnackBar(View view, String message){
+
+        Snackbar snackBar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        View snackView = snackBar.getView();
+        TextView tv = (TextView) snackView.findViewById(android.support.design.R.id.snackbar_text);
+        tv.setTextColor(Color.WHITE);
+        snackBar.show();
+
+    }
+}
