bd33b6c28af01579714790b64520a060b77f0d27
Seetharaman-mobops <sri15496@gmail.com>
2015-06-21 15:24:06 +0530

    Created layouts for news feed fragment and profile pic dialog

--- app/src/main/java/org/saarang/erp/Fragments/NewsFeedFragment.java
+++ app/src/main/java/org/saarang/erp/Fragments/NewsFeedFragment.java
@@ -1,10 +1,16 @@
 package org.saarang.erp.Fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import org.saarang.erp.R;
 
@@ -16,10 +22,36 @@ public class NewsFeedFragment extends Fragment {
     public NewsFeedFragment() {
     }
 
+    ImageView impic;
+    ImageButton ibCall;
+    ImageButton ibMail;
+    ImageButton ibProfile;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fr_news_feed, container, false);
+        View rootView = inflater.inflate(R.layout.item_news_feed, container, false);
+
+        impic = (ImageView)rootView.findViewById(R.id.imPic);
+
+        impic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater li = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                View dialoglayout = inflater.inflate(R.layout.alert_profile_dialog, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setView(dialoglayout);
+//                ibCall = (ImageButton)dialoglayout.findViewById(R.id.ibCall);
+//                ibMail = (ImageButton)dialoglayout.findViewById(R.id.ibMail);
+//                ibProfile = (ImageButton)dialoglayout.findViewById(R.id.ibProfile);
+
+                Resources r = getResources();
+                int px = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, r.getDisplayMetrics());
+                builder.show().getWindow().setLayout(px, ViewGroup.LayoutParams.WRAP_CONTENT);
+//                builder.show();
+            }
+        });
         return rootView;
     }
 }
