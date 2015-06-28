6c140091c27b6a8b487254e7dc17170813b939f5
kevin_selva_prasanna <kevinselvaprasanna@gmail.com>
2015-06-28 18:24:07 +0530

    Register GCM id with server

--- app/src/main/java/org/saarang/erp/Utils/AppConstants.java
+++ app/src/main/java/org/saarang/erp/Utils/AppConstants.java
@@ -6,8 +6,8 @@ package org.saarang.erp.Utils;
 public class AppConstants {
 
     public static String PROFILE_PICTURE = "profile_picture";
-
-
     //GCM
     public static String GCM_SENDER_ID = "474817103638";
+
+    String[] departments = {"Design & Media","Events","Facilities & Requirements","Finance","Hospitality","Marketing & Ticket Sales","Professional Shows","Publicity","QMS","Safety & Security","Sponsorship & PR","WebOps"};
 }
