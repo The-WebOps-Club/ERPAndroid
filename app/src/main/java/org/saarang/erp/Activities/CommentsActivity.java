2b82abacede6af253d7852a66c4cd88446e6bcdd
Aqel Ahammad <aqel123@gmail.com>
2015-07-11 13:50:48 +0530

    Updated with change in comments api

--- app/src/main/java/org/saarang/erp/Activities/CommentsActivity.java
+++ app/src/main/java/org/saarang/erp/Activities/CommentsActivity.java
@@ -3,15 +3,13 @@ package org.saarang.erp.Activities;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
 import org.saarang.erp.Adapters.CommentsViewPagerAdapter;
-import org.saarang.erp.Fragments.CommentsFragment;
 import org.saarang.erp.Helper.DatabaseHelper;
 import org.saarang.erp.R;
 import org.saarang.saarangsdk.Views.NonSwipeableViewPager;
 
-public class CommentsActivity extends FragmentActivity implements CommentsFragment.CommentsListener{
+public class CommentsActivity extends FragmentActivity {
 
     CommentsViewPagerAdapter pageAdapter;
     String comments, acknowledgment, postId, postDate;
@@ -42,7 +40,6 @@ public class CommentsActivity extends FragmentActivity implements CommentsFragme
         mpager=(NonSwipeableViewPager)findViewById(R.id.vpComments);
         mpager.setAdapter(pageAdapter);
 
-        CommentsFragment.setOnChangeListener(this);
 
     }
 
@@ -50,10 +47,4 @@ public class CommentsActivity extends FragmentActivity implements CommentsFragme
         mpager.setCurrentItem(target);
     }
 
-
-    @Override
-    public void onCommentsAdded() {
-        mpager.setCurrentItem(0);
-        Log.d(LOG_TAG, "Comment added");
-    }
 }
--- app/src/main/java/org/saarang/erp/Activities/SinglePostActivity.java
+++ app/src/main/java/org/saarang/erp/Activities/SinglePostActivity.java
@@ -3,20 +3,16 @@ package org.saarang.erp.Activities;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.saarang.erp.Adapters.CommentsAdapter;
-import org.saarang.erp.Adapters.PeopleAdapter;
 import org.saarang.erp.Adapters.SinglePostAdapter;
 import org.saarang.erp.Helper.DatabaseHelper;
 import org.saarang.erp.Objects.ERPComment;
@@ -30,7 +26,7 @@ import org.saarang.saarangsdk.Objects.PostParam;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SinglePostActivity extends ActionBarActivity {
+public class SinglePostActivity extends AppCompatActivity {
 
     static ERPPost post;
     static List<ERPComment> commentsList;
